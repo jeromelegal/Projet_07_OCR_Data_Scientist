9e36a16b2677a2ae9e906f40e3b7cd5d51e44574
garthcrow <garth@phylcero.fr>
2024-11-25 08:08:11 +0000

    Add pydantic to ensure data compliance for MLFlow

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,21 +1,30 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
 import requests
+from pydantic import BaseModel
+
+class PredictionRequest(BaseModel):
+    dataframe_split: dict
 
 app = FastAPI()
 
-# validate inputs
-class PredictionRequest(BaseModel):
-    instances: list
-
-# mlflow url
+# URL de MLflow
 MLFLOW_URL = "http://192.168.2.189:5000/invocations"
-
 
 @app.post("/predict")
 async def predict(data: PredictionRequest):
-    response = requests.post(MLFLOW_URL, json=data.dict())
-    if response.status_code == 200:
-        return {"predictions": response.json()}
-    else:
-        return {"error": response.text}
+    try:
+        # Envoyer les données à MLflow
+        response = requests.post(MLFLOW_URL, json=data.dict())
+
+        # Vérifier le statut de la réponse
+        if response.status_code != 200:
+            raise HTTPException(
+                status_code=response.status_code,
+                detail=f"Erreur depuis MLflow: {response.text}"
+            )
+
+        # Retourner la réponse de MLflow
+        return response.json()
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
