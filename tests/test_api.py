906e9019979817af846d09f6817c2243a534654c
garthcrow <garth@phylcero.fr>
2024-11-29 17:52:14 +0000

    Test file to test API with Pytest

---
+++ tests/test_api.py
@@ -0,0 +1,76 @@
+import requests
+import pytest
+import pandas as pd
+from unittest.mock import patch
+from api.main import format_data_for_api, predict
+import json
+
+##############################################################################################
+### variables
+
+API_URL = "https://ocr-projet07-jlg-dze9cfcrbzadb4av.germanywestcentral-01.azurewebsites.net"
+MLFLOW_URL = "http://mlflowjlg-container.centralus.azurecontainer.io:5000/invocations"
+
+@pytest.fixture
+def expected_json():
+    expected_json_path = "./fixtures/expected_data.json"
+    with open(expected_json_path, "r") as json_file:
+        return json.load(json_file)
+
+##############################################################################################
+### test functions 
+
+# verify if Azure Web App is correctly booted and API accessible
+def test_app_started():
+    try:
+        response = requests.get(API_URL)  
+        assert response.status_code == 200, "API is not accessible (status not : 200)."
+        assert "Streamlit" in response.text, "Streamlit is not correctly booted"
+        #assert "Prêt à dépenser" in response.text, "Streamlit is not correctly booted"
+
+    except requests.exceptions.RequestException as e:
+        pytest.fail(f"L'application n'est pas accessible : {e}")
+
+# verify if ACR is correctly booted accessible
+def test_container_accessibility(expected_json):
+    try:
+
+        response = requests.post(MLFLOW_URL, json=expected_json)  
+        assert response.status_code == 200, "MLflow container is not accessible."
+    except requests.exceptions.RequestException as e:
+        pytest.fail(f"Impossible to connect to MLflow container : {e}")
+
+# testing format function in API
+def test_format_data_for_api(expected_json):
+    # load test_data file
+    test_csv_path = "./fixtures/test_data.csv"
+    df = pd.read_csv(test_csv_path)
+
+    result = format_data_for_api(df)
+
+    # verify json file
+    assert result == expected_json, "JSON formatting is not correct."
+
+# testing predict function and verify the 2 prediction results
+def test_predict():
+    # load test_data file
+    test_csv_path = "./fixtures/test_data.csv"
+    df = pd.read_csv(test_csv_path)
+
+    # JSON convert
+    test_data = format_data_for_api(df)
+
+    try:
+        # request
+        response = requests.post(MLFLOW_URL, json=test_data)
+        assert response.status_code == 200, "MLflow container did not send statut HTTP 200."
+        predictions = response.json().get("predictions")
+        assert predictions is not None, "There is no prediction."
+
+        # Vérifier les prédictions attendues (0 et 1)
+        assert len(predictions) == 2, "Quantity of predisction is incorrect."
+        assert predictions[0] == 0, "First prediction is Fail, must be : 0."
+        assert predictions[1] == 1, "Second prediction is Fail, must be : 1."
+
+    except requests.exceptions.RequestException as e:
+        pytest.fail(f"Issue in communication with MLflow container : {e}")
