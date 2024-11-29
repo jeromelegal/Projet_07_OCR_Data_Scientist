f13b3fd79bd87926361da20b4f2bff10e22faf0c
garthcrow <garth@phylcero.fr>
2024-11-29 14:19:11 +0000

    Test model file to verify Class=1 mean predict_proba is < 0.1

---
+++ tests/test_model.py
@@ -0,0 +1,23 @@
+import pytest
+import pandas as pd
+import mlflow.pyfunc
+import numpy as np
+
+
+# import TEST dataset
+@pytest.fixture(scope="module")
+def load_data():
+    data_path = "../data/source"
+    df = pd.read_csv(f"{data_path}/application_test.csv")
+    return df 
+
+# import model once
+@pytest.fixture(scope="module")
+def model():
+    artifact_path = "../mlflow/artifacts/LGBM_production_pipeline_model/"
+    return mlflow.pyfunc.load_model(artifact_path)
+
+
+def test_model_prediction(model, load_data):
+    prediction = model.predict(load_data)
+    assert (np.mean(prediction) < 0.1)
