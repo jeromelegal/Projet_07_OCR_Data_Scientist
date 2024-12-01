2ee62b85c69cfa1d158a277e545f7b03c6c3a6ab
garthcrow <garth@phylcero.fr>
2024-12-01 14:49:39 +0000

    Test API issues

--- api/startup.sh
+++ api/startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pip install --no-cache-dir -r /home/site/wwwroot/api/requirements.txt
+export PYTHONPATH=$PYTHONPATH:/home/site/wwwroot/packages
 streamlit run /home/site/wwwroot/api/app.py --server.port=$PORT --server.address=0.0.0.0
