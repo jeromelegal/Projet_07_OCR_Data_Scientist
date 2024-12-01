e123853cc3f726f072872a5e7ed58837b96b0909
garthcrow <garth@phylcero.fr>
2024-12-01 14:17:28 +0000

    Test API issues

--- api/startup.sh
+++ api/startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pip install --no-cache-dir -r api/requirements.txt
-streamlit run api/app.py --server.port=$PORT --server.address=0.0.0.0
+pip install --no-cache-dir -r /home/site/wwwroot/api/requirements.txt
+streamlit run /home/site/wwwroot/api/app.py --server.port=$PORT --server.address=0.0.0.0
